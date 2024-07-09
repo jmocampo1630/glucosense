938dc1e64e84c7a3dc0365c886ecaa8cda110ba8
Michael Ocampo <mocampo@SygnalPartners.com>
2024-07-09 22:39:10 +0800

    splash

--- lib/main.dart
+++ lib/main.dart
@@ -27,12 +27,12 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF37B5B6)),
         useMaterial3: true,
       ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color(0xFF37B5B6), brightness: Brightness.dark),
-        useMaterial3: true,
-      ),
+      // darkTheme: ThemeData(
+      //   brightness: Brightness.dark,
+      //   colorScheme: ColorScheme.fromSeed(
+      //       seedColor: const Color(0xFF37B5B6), brightness: Brightness.dark),
+      //   useMaterial3: true,
+      // ),
       themeMode: ThemeMode.system, // Use system theme mode
       home: MyHomePage(title: 'GlucoLook', camera: camera),
       // home: PatientRecordPage(title: 'GlucoLook', camera: camera),
